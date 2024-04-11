3496f7fc51b4d1dbd8b063c936eded5d80cf6876
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2024-04-11 11:19:50 +0100

    Add project files.

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,101 @@
+﻿namespace BinarySearchTrees
+{
+
+
+    class BinarySearchTree
+    {
+        public class Node
+        {
+            public Node left;
+            public Node right;
+            public int value;
+            public int hight;
+        }
+
+        public Node root;
+
+        public void NodeInitizled()
+        {
+            if (root == null)
+            {
+                root = new Node()
+                {
+                    value = 0,
+                    hight = 0,
+                };
+            }
+        }
+
+        public void insert(Node node)
+        {
+            insert(root, node);
+        }
+
+        private void insert(Node PNode, Node CNode)
+        {
+            if (CNode.value <= PNode.value)
+            {
+                if (PNode.left != null)
+                {
+                    insert(PNode.left, CNode);
+                }
+                else
+                {
+                    PNode.left = CNode;
+                }
+            }
+
+            else
+            {
+                if (PNode.right != null)
+                {
+                    insert(PNode.right, CNode);
+                }
+                else
+                {
+                    PNode.right = CNode;
+                }
+            }
+
+            if (PNode.left != null && PNode.right != null)
+            {
+                PNode.hight = Math.Max(PNode.left.hight + 1, PNode.right.hight + 1);
+            }
+            else if (PNode.left != null)
+            {
+                PNode.hight = PNode.left.hight + 1;
+            }
+            else
+            {
+                PNode.hight = PNode.right.hight + 1;
+            }
+        }
+
+        public List<Tuple<Node, Node>> CheckBalance()
+        {
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            List<Tuple<Node, Node>> targetNodes = new List<Tuple<Node, Node>>();
+            Node currentNode = null;
+            while (queue.Count > 0)
+            {
+                currentNode = queue.Dequeue();
+                if (currentNode.left != null && currentNode.right != null)
+                {
+                    if (Math.Abs(currentNode.left.hight - currentNode.right.hight) >= 2)
+                    {
+                        Node maxNode = currentNode.right.hight > currentNode.left.hight ? currentNode.right : currentNode.left;
+                        targetNodes.Add(new Tuple<Node, Node>(currentNode, maxNode));
+                    }
+                    else
+                    {
+                        queue.Enqueue(currentNode.left);
+                        queue.Enqueue(currentNode.right);
+                    }
+                }
+            }
+
+            return targetNodes;
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,61 @@
+﻿using BinarySearchTrees;
+using System.Xml.Linq;
+
+BinarySearchTree binarySearchTree = new BinarySearchTree();
+binarySearchTree.NodeInitizled();
+
+Console.WriteLine(binarySearchTree.root.value);
+
+BinarySearchTree.Node left1 = new BinarySearchTree.Node()
+{
+    value = -1
+};
+
+binarySearchTree.insert(left1);
+
+BinarySearchTree.Node right1 = new BinarySearchTree.Node()
+{
+    value = 1
+};
+
+binarySearchTree.insert(right1);
+
+BinarySearchTree.Node left2 = new BinarySearchTree.Node()
+{
+    value = -2
+};
+
+binarySearchTree.insert(left2);
+
+BinarySearchTree.Node left3 = new BinarySearchTree.Node()
+{
+    value = -3
+};
+
+binarySearchTree.insert(left3);
+
+BinarySearchTree.Node left4 = new BinarySearchTree.Node()
+{
+    value = -4
+};
+
+binarySearchTree.insert(left4);
+
+var a = binarySearchTree.CheckBalance();
+
+
+Console.WriteLine(binarySearchTree.root.left.value);
+
+Console.WriteLine(binarySearchTree.root.right.value);
+/*
+
+BinarySearchTree.Node left1 = new BinarySearchTree.Node()
+{
+    value = 1
+};
+
+BinarySearchTree.Node right1 = new BinarySearchTree.Node()
+{
+    value = 2
+};
+*/
